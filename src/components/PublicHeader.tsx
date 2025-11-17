cfb154bde8b4cb13cf838c264d95a963ff5b5768
emaLeal <emanuel.leal@correounivalle.edu.co>
2025-11-17 15:25:57 -0500

    Public Header Component Ready(Missing routes)

--- src/components/PublicHeader.tsx
+++ src/components/PublicHeader.tsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router";
+import logo from "/logo.jpeg";
+
 export default function PublicHeader() {
+    const routes = [
+        { label: "Inicio", path: "" },
+        { label: "Sobre nosotros", path: "" },
+        { label: "Unirse a una reunión", path: "" },
+        { label: "Ayuda", path: "" },
+    ];
     return (
-        <header>
-            <h1>Header publico</h1>
+        <header className="flex flex-col w-full p-2 sm:flex-row-reverse sm:justify-between">
+            <div className="flex justify-end gap-2 sm:gap-2">
+                <button className="bg-linear-to-r from-[#7B76F1] to-[#4A44ED] p-4 rounded-full text-white font-semibold text-xs sm:my-6 sm:text-base cursor-pointer">
+                    Registrarse
+                </button>
+                <button className="bg-linear-to-r from-[#7B76F1] to-[#4A44ED] p-2 rounded-full text-white font-semibold text-xs sm:my-6 cursor-pointer sm:text-base">
+                    Iniciar sesión
+                </button>
+            </div>
+            <div className="flex flex-row">
+                <Link to={"/"}>
+                    <img
+                        src={logo}
+                        className="cursor-pointer w-[53px] h-[44px] sm:w-[103px] sm:h-[87px]"
+                        alt="logo"
+                    />
+                </Link>
+                <nav className="sm:mx-52">
+                    <ul className="flex flex-row ml-2 my-2 sm:my-6 content-center gap-2">
+                        {routes.map((route, index) => (
+                            <li key={index}>
+                                <Link
+                                    to={route.path}
+                                    className="cursor-pointer text-xs text-[#4A44ED] sm:text-lg sm:mx-2"
+                                >
+                                    {route.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
