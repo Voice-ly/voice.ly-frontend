607171abd2bfb62c0658931b7e290791ffa21d6f
emaLeal <emanuel.leal@correounivalle.edu.co>
2025-12-10 19:00:52 -0500

    Small change

--- src/pages/MeetingPage.tsx
+++ src/pages/MeetingPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect, useCallback } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useUserStore } from "../stores/useUserStore";
 import { socket } from "../lib/Socket";
 // @ts-ignore
